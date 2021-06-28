e827719a80b5dda4264c8e76de20a86d41cc19f7
Anastasia <nastasia199126@gmail.com>
2021-06-28 15:22:52 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,54 @@
+import debounce from 'lodash.debounce';
 import './sass/main.scss';
-// import fetchCountries from './js/fetchCountries';
-//
-import { receiveCountries } from './js/fetchCountries';
+import receiveCountries from './js/fetchCountries';
+import listTpl from './templates/list-countries.hbs';
+import cardCountri from './templates/card-countri.hbs';
+
+import { alert, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+
+defaultModules.set(PNotifyMobile, {});
+
+const input = document.querySelector('.input');
+const list = document.querySelector('.list');
+const wrapper = document.querySelector('.wrapper');
+
+input.addEventListener('input', onInputInput);
+
+function onInputInput(event) {
+  event.preventDefault();
+
+  console.log(event.currentTarget);
+  const name = input.value;
+
+  console.log(name);
+  receiveCountries(name)
+    .then(debounce(renderCountrys, 500))
+    .catch(error => console.log(error));
+}
+
+function renderCountrys(arrayCountrys) {
+  console.log(arrayCountrys);
+  let listCountrys = listTpl(arrayCountrys);
+  let markupCardCountri = cardCountri(arrayCountrys);
+  if (arrayCountrys.length === 1) {
+    list.innerHTML = '';
+    wrapper.innerHTML = markupCardCountri;
+    console.log(markupCardCountri);
+  }
+  if (arrayCountrys.length <= 10 && arrayCountrys.length >= 2) {
+    list.innerHTML = listCountrys;
+  }
+  if (arrayCountrys.length > 10) {
+    alert({
+      type: 'error',
+      text: 'Too many matches found. Please enter a more specific Query!',
+      closerHover: true,
+      delay: 2000,
+    });
+  }
+}
